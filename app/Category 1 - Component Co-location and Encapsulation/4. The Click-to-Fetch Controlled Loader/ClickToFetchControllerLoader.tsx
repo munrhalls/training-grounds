5a08507a1a10e7993e36cc5d482cf6761f11b17a
Munrhalls <antarcticdepths71@gmail.com>
2025-10-29 18:50:25 +0100

    exercise C1.4.

--- app/Category 1 - Component Co-location and Encapsulation/4. The Click-to-Fetch Controlled Loader/ClickToFetchControllerLoader.tsx
+++ app/Category 1 - Component Co-location and Encapsulation/4. The Click-to-Fetch Controlled Loader/ClickToFetchControllerLoader.tsx
@@ -21,17 +21,64 @@
  *          1) A 'Start Fetch' button that increments the trigger state
  *          2) A 'Cancel' button (visible only during loading) that resets the UI to the idle status
  */
+// const halfChance = () => Math.floor(Math.random() * 2);
+//         const chance = halfChance();
+
+import { useState, useEffect } from "react";
 
 const CancellableDataFetcher = function () {
-  // state
-  // fetch inside hook
-  // defensive coding around fetch
-  // clean up if dependencies change or component unmounts
-  // ui res
+  const [isFetching, setIsFetching] = useState(false);
+  const [data, setData] = useState(null;
+  // fetching
+  // cancellable
+
+  const fetchData = async function () {
+    try {
+      const fetchingCargo = {
+        status: 200,
+        data: {
+          items: 5,
+          brand: "Some brand",
+        },
+      };
+
+      const fetchPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(fetchingCargo);
+        }, 3000);
+      });
+
+      const data = await fetchPromise;
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+
+  }, [isFetching]);
+
+  const triggerFetch = function () {
+    setIsFetching(true);
+  };
 
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <button className="p-4 m-4 border-2 border-black rounded-md bg-blue-600 text-white font-black">Fetch data</button>
+        {data && <>
+                <h1>Data: </h1>
+                <div>
+                    <label>Items: </label>
+                    <span>{data.items}</span>
+                    <label>Brand: </label>
+                    <span>{data.brand} </span>
+                </div>
+            </>
+        }
+
+      <button onClick={triggerFetch} className="p-4 m-4 border-2 border-black rounded-md bg-blue-600 text-white font-black">
+        Fetch data
+      </button>
       <button className="p-4 m-4 border-2 border-black rounded-md bg-red-600 text-white font-black">Cancel</button>
     </div>
   );
