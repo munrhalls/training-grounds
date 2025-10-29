f96ebc403d1ec3dff33d6308e48032e90ef23842
Munrhalls <antarcticdepths71@gmail.com>
2025-10-29 17:43:13 +0100

    exercise C1.3.

--- app/Category 1 - Component Co-location and Encapsulation/3. The Self-Contained Loader/SelfContainedLoader.tsx
+++ app/Category 1 - Component Co-location and Encapsulation/3. The Self-Contained Loader/SelfContainedLoader.tsx
@@ -15,12 +15,73 @@
  * - Primitive Focus: Prove the component can own all aspects of its data lifecycle—fetching, state, and rendering—without external state management
  */
 
+import { useState, useEffect } from "react";
+
 const SelfContainedLoader = function () {
+  const [status, setStatus] = useState("loading");
+  const [user, setUser] = useState(null);
+
+  const fetchUserData = async function () {
+    const dataPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const halfChance = () => Math.floor(Math.random() * 2);
+        const chance = halfChance();
+
+        if (chance) {
+          const userData = {
+            status: 200,
+            name: "Super User",
+            email: "extremelyawesome@email.com",
+            brand: "Best",
+            coolStatus: "Cool",
+          };
+          resolve(userData);
+        } else {
+          reject("Error message: request failed.");
+        }
+      }, 100);
+    });
+
+    try {
+      const data = await dataPromise;
+      setUser(data);
+      console.log(data);
+      setStatus("success");
+      return data;
+    } catch {
+      setStatus("error");
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
     <div className="h-screen w-full flex items-center justify-center">
       <div className="border-black  border-2 rounded-lg p-8 h-3/4 w-3/4 min-w-4/5 grid place-content-center">
-        <div className="animate-spin w-8 h-8 border-4 border-t-blue-500 border-gray-200 rounded-full" />
+        {status === "loading" && <div className="animate-spin w-8 h-8 border-4 border-t-blue-500 border-gray-200 rounded-full" />}
+        {status !== "loading" && (
+          <h1>
+            Status: <span className={status === "success" ? "text-green-500 font-black uppercase" : "text-red-600 font-black"}>{status}</span>
+          </h1>
+        )}
+        {status === "error" && <div className="p-8 border-4 border-t-red-500 border-red-500 rounded-full text-red-600">Error </div>}
+
+        {status === "success" && user && (
+          <div className="border-2 border-black rounded-md p-4 my-12 ">
+            {Object.entries(user).map(([key, val]) => {
+              return (
+                <div key={key}>
+                  <label className="font-black">{key}: </label>
+                  <span>{val}</span>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
+      {status === "error" && <div className=""></div>}
     </div>
   );
 };
