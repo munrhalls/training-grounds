66f7faa4efef9726a637c903aa9f838ae646e6d5
Munrhalls <antarcticdepths71@gmail.com>
2025-10-29 13:12:50 +0100

    exercise C1.1.

--- app/Category 1 - Component Co-location and Encapsulation/1. The Single Feature Unit/ToggleAndDisplayUnit.tsx
+++ app/Category 1 - Component Co-location and Encapsulation/1. The Single Feature Unit/ToggleAndDisplayUnit.tsx
@@ -16,20 +16,22 @@ import { useState } from "react";
 const TooltipFeature = function () {
   const [isTooltipVisible, setIsTooltipVisible] = useState(false);
   return (
-    <div className="w-full h-screen grid place-content-center">
-      {isTooltipVisible && (
-        <div className=" border-black border-4 rounded-3xl text-black p-4">
-          <h1>Tooltip title</h1>
-          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore saepe cum cupiditate harum, in non sed, nostrum dicta fuga deserunt ullam, nesciunt quia incidunt. Odio magni necessitatibus molestiae dicta facilis! </p>
-          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore saepe cum cupiditate harum, in non sed, nostrum dicta fuga deserunt ullam, nesciunt quia incidunt. Odio magni necessitatibus molestiae dicta facilis! </p>
-          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore saepe cum cupiditate harum, in non sed, nostrum dicta fuga deserunt ullam, nesciunt quia incidunt. Odio magni necessitatibus molestiae dicta facilis! </p>
-        </div>
-      )}
+    <>
+      <div className="w-full grid place-content-center">
+        {isTooltipVisible && (
+          <div className=" border-black border-4 rounded-3xl text-black p-4">
+            <h1>Tooltip title</h1>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore saepe cum cupiditate harum, in non sed, nostrum dicta fuga deserunt ullam, nesciunt quia incidunt. Odio magni necessitatibus molestiae dicta facilis! </p>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore saepe cum cupiditate harum, in non sed, nostrum dicta fuga deserunt ullam, nesciunt quia incidunt. Odio magni necessitatibus molestiae dicta facilis! </p>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore saepe cum cupiditate harum, in non sed, nostrum dicta fuga deserunt ullam, nesciunt quia incidunt. Odio magni necessitatibus molestiae dicta facilis! </p>
+          </div>
+        )}
+      </div>
 
-      <button onClick={() => setIsTooltipVisible((is) => (is ? false : true))} className="button-primary bg-blue-600 rounded-lg p-4 text-white font-black cursor-pointer">
+      <button onClick={() => setIsTooltipVisible((is) => (is ? false : true))} className="w-full grid place-content-center button-primary bg-blue-600 rounded-lg p-4 text-white font-black cursor-pointer">
         {isTooltipVisible ? "SHOW" : "HIDE"}
       </button>
-    </div>
+    </>
   );
 };
 export default TooltipFeature;
