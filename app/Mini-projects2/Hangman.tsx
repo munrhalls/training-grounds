bbd193d7639db0b3548766270d811f610197b0d6
Munrhalls <antarcticdepths71@gmail.com>
2025-11-08 17:12:22 +0100

    Hangman - solved less crude looking css but incomplete yet - progress step achieved - rotating two rods around middle

--- app/Mini-projects2/Hangman.tsx
+++ app/Mini-projects2/Hangman.tsx
@@ -4,8 +4,10 @@ export default function Hangman() {
   return (
     <div className="h-screen w-screen grid place-content-center">
       <div className="relative w-4 h-96">
-        <div className="absolute top-2 right-2 bg-black h-4 w-48 origin-bottom-right rotate-30  "></div>
-        <div className="absolute   bg-black -top-20 right-2 h-4  w-48 mr-40 origin-top-right -rotate-30 "></div>
+        <div className="absolute top-0 right-0 w-96 h-4 flex flex-row items-end">
+          <div className="bg-black h-4 w-1/2 origin-bottom-right -rotate-30"></div>
+          <div className="bg-black h-4 w-1/2 origin-bottom-left rotate-30"></div>
+        </div>
         <div className="absolute top-0 bottom-0 w-4 bg-black"></div>
       </div>
       <div>hangman</div>
