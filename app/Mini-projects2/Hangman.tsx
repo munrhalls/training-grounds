108bb0fb06fbbc4216646978a0560ad7ca605caf
Munrhalls <antarcticdepths71@gmail.com>
2025-11-08 16:59:56 +0100

    Hangman - solved crude looking css with triangular dome

--- app/Mini-projects2/Hangman.tsx
+++ app/Mini-projects2/Hangman.tsx
@@ -3,9 +3,10 @@
 export default function Hangman() {
   return (
     <div className="h-screen w-screen grid place-content-center">
-      <div className="relative">
-        <div className="absolute -rotate-45 -top-45 left-0 border-t-black border-t-4 h-72 w-72 "></div>
-        <div className="w-4 h-72 bg-black"></div>
+      <div className="relative w-4 h-96">
+        <div className="absolute top-2 right-2 bg-black h-4 w-48 origin-bottom-right rotate-30  "></div>
+        <div className="absolute   bg-black -top-20 right-2 h-4  w-48 mr-40 origin-top-right -rotate-30 "></div>
+        <div className="absolute top-0 bottom-0 w-4 bg-black"></div>
       </div>
       <div>hangman</div>
     </div>
