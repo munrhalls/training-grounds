10e8fa19b971013eb0b1b48eab4d5f6c072b6e4d
Munrhalls <antarcticdepths71@gmail.com>
2025-11-08 19:10:19 +0100

    // for debrief pruposes - that was time to eat dinner, now 19;00 starting again ///// Hangman - make hangman parts be derived from partsLeft state that starts from 6 and goes to 0, at which its game over

--- app/Mini-projects2/Hangman.tsx
+++ app/Mini-projects2/Hangman.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function Hangman() {
+  const [partsLeft, setPartsLeft] = useState(6);
+
   return (
     <div className="h-screen flex flex-col items-center justify-center">
       <div className="h-4/5 w-full max-w-sm relative">
@@ -7,12 +11,15 @@ export default function Hangman() {
         <div className="absolute top-0 h-16 w-2 right-1/4 mr-2 bg-black"></div>
 
         <div className="absolute top-16 left-[calc(75%-0.75rem)]">
-          <div className="absolute top-0 h-12 w-12 border-4 border-black rounded-full left-0 -translate-x-1/2"></div>
-          <div className="absolute top-12 h-24 w-4 bg-black left-0 -translate-x-1/2"></div>
-          <div className="absolute top-12 h-16 w-4 bg-black origin-top-right rotate-45 right-1"></div>
-          <div className="absolute top-12 h-16 w-4 bg-black origin-top-left -rotate-45 left-1"></div>
-          <div className="absolute top-36 h-16 w-4 bg-black origin-top-right rotate-45 -right-1"></div>
-          <div className="absolute top-36 h-16 w-4 bg-black origin-top-left -rotate-45 -left-1"></div>
+          {/* each part needs to start hidden */}
+          {/* un-hiding needs to proceed part by part as partsLeft state changes from 6-- to 0 */}
+
+          <div className={`${!(partsLeft < 1) && "hidden"} absolute top-0 h-12 w-12 border-4 border-black rounded-full left-0 -translate-x-1/2`}></div>
+          <div className={`${!(partsLeft < 2) && "hidden"} absolute top-12 h-24 w-4 bg-black left-0 -translate-x-1/2`}></div>
+          <div className={`${!(partsLeft < 3) && "hidden"} absolute top-12 h-16 w-4 bg-black origin-top-right rotate-45 right-1`}></div>
+          <div className={`${!(partsLeft < 4) && "hidden"} absolute top-12 h-16 w-4 bg-black origin-top-left -rotate-45 left-1`}></div>
+          <div className={`${!(partsLeft < 5) && "hidden"} absolute top-36 h-16 w-4 bg-black origin-top-right rotate-45 -right-1`}></div>
+          <div className={`${!(partsLeft < 6) && "hidden"} absolute top-36 h-16 w-4 bg-black origin-top-left -rotate-45 -left-1`}></div>
         </div>
       </div>
     </div>
